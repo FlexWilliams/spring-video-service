471df5d95c2e8989d0fca901e919554431acacca
Alex Williams <work.alexwilliams@proton.me>
2023-08-14 22:46:15 -0600

    Upgrading to Java 17 and gradle 8
    - Removing MVC style controller in favor for rest controller in PageController

--- src/main/kotlin/sample/service/video/PageController.kt
+++ src/main/kotlin/sample/service/video/PageController.kt
@@ -1,7 +1,8 @@
 package sample.service.video
 
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -9,10 +10,11 @@ import java.nio.file.Files
 import java.nio.file.Paths
 import kotlin.streams.toList
 
-@Controller
+@RestController
+@RequestMapping("/")
 class PageController(@Value("\${video.location}") val videoLocation: String) {
 
-	@GetMapping("/")
+	@GetMapping()
 	fun index(model: Model): String {
 		// getting all of the files in video folder
 		val videos = Files.list(Paths.get(videoLocation)).map { it.fileName.toString() }.toList()
